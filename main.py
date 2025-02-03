c2073321640d64e54d74079182410f5aea8426e0
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2025-02-03 17:42:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# load models
+
+# load video
+
+# detect vehicles
+       
+# track vehicles
+       
+# detect license plates
+       
+# assign license plate to car
+          
+# crop license plate
+
+# process license plate
+                
+# read license plate number
+    
+# write results
