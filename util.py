b9c1c4e7ebe39df7854c5890c8ef8ce104227cab
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2025-02-06 17:03:30 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,1 +1,10 @@
+#import library
 
+# Initialize the OCR reader
+
+
+# Mapping dictionaries for character conversion
+# Some characters in license plates might be misinterpreted as numbers or vice versa
+
+#create Function
+#
