51ab48a5b92363d81b6b2165ef80cbb00203c0c5
sanskrutikukadiya3303 <165458766+sanskrutikukadiya3303@users.noreply.github.com>
2025-02-07 18:33:29 +0530

    Update util.py

--- util.py
+++ util.py
@@ -9,17 +9,59 @@
 #create Function
  # Save results to a CSV file
 def write_csv():
+  """
+    Write the results to a CSV file.
 
+    Args:
+        results (dict): Dictionary containing the results.
+        output_path (str): Path to the output CSV file.
+    """
  #Check if the license plate format is correct.
  def license_complies_format():
+    """
+    Check if the license plate text complies with the required format.
 
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
  # Fix incorrect characters in the license plate text.
  def format_license():
+  """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
 
  #Read and recognize text from a license plate image.
  def read_license_plate():
+  """
+    Read the license plate text from the given cropped image.
 
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
+
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
  #Find the car associated with the license plate.
  def get_car():
+   get_car(license_plate, vehicle_track_ids):
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
   
 
