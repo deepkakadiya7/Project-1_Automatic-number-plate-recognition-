417d10816b82c9e3e02639f042e4e75a0eed589b
sanskrutikukadiya3303 <165458766+sanskrutikukadiya3303@users.noreply.github.com>
2025-02-07 16:57:51 +0530

    Update util.py

--- util.py
+++ util.py
@@ -7,8 +7,19 @@
 # Some characters in license plates might be misinterpreted as numbers or vice versa
 
 #create Function
- # Save results to a CSV file.
+ # Save results to a CSV file
+def write_csv():
+
  #Check if the license plate format is correct.
+ def license_complies_format():
+
  # Fix incorrect characters in the license plate text.
+ def format_license():
+
  #Read and recognize text from a license plate image.
+ def read_license_plate():
+
  #Find the car associated with the license plate.
+ def get_car():
+  
+
