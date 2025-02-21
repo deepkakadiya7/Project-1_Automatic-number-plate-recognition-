e6642415da8f5a0de4b3d4de533eb224cb90f70a
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2025-02-21 14:34:06 +0530

    Update add_missing_data.py

--- add_missing_data.py
+++ add_missing_data.py
@@ -15,7 +15,14 @@ def interpolate_bounding_boxes(data):
         frame_numbers_ = [p['frame_nmr'] for p in data if int(float(p['car_id'])) == int(float(car_id))]
         print(frame_numbers_, car_id)
 
+         # Filter data for a specific car ID
+        car_mask = car_ids == car_id
+        car_frame_numbers = frame_numbers[car_mask]
+        car_bboxes_interpolated = []
+        license_plate_bboxes_interpolated = []
 
+        first_frame_number = car_frame_numbers[0]
+        last_frame_number = car_frame_numbers[-1]  
 
   return interpolated_data
 
